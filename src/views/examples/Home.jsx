c64a25987f512c43cb0725f32db7837b3d7fe7a1
dadyilhawallacevidal <wallace.vidal@dadyilha.com.br>
2023-07-04 16:44:17 -0300

    Fist Commit

---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = props => {
+
+    return (
+        <aside className="Menu">
+            Componente Menu
+        </aside>);
+}
+
+
+
+export default Home;
